56864ae58192e70ac8fb15f8801adb84bef014d9
Leon Brettin <l.brettin@tu-braunschweig.de>
2019-04-23 16:30:01 +0200

    Working on points and name for the creation of a new room

--- app/src/main/java/de/tubs/campusjagd/view/CreateNewRoomFragment.java
+++ app/src/main/java/de/tubs/campusjagd/view/CreateNewRoomFragment.java
@@ -1,5 +1,6 @@
 package de.tubs.campusjagd.view;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,9 +9,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import de.tubs.campusjagd.MainActivity;
 import de.tubs.campusjagd.R;
@@ -44,24 +47,70 @@ public class CreateNewRoomFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 // Create a new room and save it
-                CreateNewRoomFragment.this.createNewRoom();
+                if (CreateNewRoomFragment.this.createNewRoom()) {
+                    // Go back to the previous fragment
+                    try {
+                        hideKeyboard(CreateNewRoomFragment.this.getActivity());
 
-                // Go back to the previous fragment
-                try {
-                    CreateNewRoomFragment.this.getActivity().getSupportFragmentManager().popBackStackImmediate();
-                } catch (NullPointerException e) {
-                    Logger.LogExeption(CreateNewRoomFragment.class.getSimpleName(), "Error while going back to previous class", e);
+                        CreateNewRoomFragment.this.getActivity().getSupportFragmentManager().popBackStackImmediate();
+                    } catch (NullPointerException e) {
+                        Logger.LogExeption(CreateNewRoomFragment.class.getSimpleName(), "Error while going back to previous class", e);
 
-                    // We dont want our app to get stuck so we start the main activity (should not happen but just to be sure)
-                    MainActivity.startActivty(v.getContext());
+                        // We dont want our app to get stuck so we start the main activity (should not happen but just to be sure)
+                        MainActivity.startActivty(v.getContext());
+                    }
                 }
             }
         });
     }
 
-    private void createNewRoom() {
+    /**
+     * Creates a new room and saves it in the resources
+     * @return True, when all values are good and the room is saved successfully.
+     */
+    private boolean createNewRoom() {
+        boolean successFull = true;
+
+        // Get roomName
         String roomName = mNameEditText.getText().toString();
+        if (roomName.equals("")){
+            successFull = false;
+            Toast.makeText(this.getContext(), R.string.create_room_no_name_error, Toast.LENGTH_LONG).show();
+        }
+
+        // Get points
+        try {
+            int points = Integer.parseInt(mPointsEditText.getText().toString());
+
+        } catch (NumberFormatException e) {
+
+            // If there is an error with the points, give the user a toast and clear the edit text
+            Logger.LogExeption(CreateNewRoomFragment.class.getSimpleName(), "Error while parsing points", e);
+            Toast.makeText(this.getContext(), R.string.create_room_parse_point_error, Toast.LENGTH_LONG).show();
+            mPointsEditText.getText().clear();
+
+            successFull = false;
+        }
 
 
+
+        return successFull;
+    }
+
+    /**
+     * Method from:
+     * https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+     * to hide the keyboard
+     * @param activity The current activity
+     */
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
