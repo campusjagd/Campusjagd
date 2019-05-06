a89bd8c8d1c685216e043b0bfae1cc50414e1d9a
Chamaeleon <y0067315@tu-bs.de>
2019-05-06 10:39:40 +0200

    added get challenges

--- server/app/api/challenges.py
+++ server/app/api/challenges.py
@@ -6,8 +6,27 @@ from app.api.errors import bad_request
 
 @bp.route('/challenges', methods=['GET'])
 def get_challenges():
-    pass
+    data = {
+        "get_challenges": [challenge.to_dict() for challenge in db.session.query(Challenge).all()],
+        "number_of_challenges": db.session.query(Challenge).count()
+    }
+    return jsonify(data)
 
 @bp.route('/challenges', methods=['POST'])
 def create_challenge():
-    pass
+    data = request.get_json() or {}
+    if 'name' not in data:
+        return bad_request('must include challengename')
+    if db.session.query(Challenge).filter_by(name=data['name'].upper()).first():
+        return bad_request('challenge aready existing')
+    else:
+        # add challenge
+        challenge = Challenge()
+        challenge.name = data['name']
+        # challenge.rooms
+        db.session.add(challenge)
+        db.session.commit()
+    response = jsonify(challenge.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.create_challenge', id=challenge.id)
+    return response
