590673c22fa7de2eac2ee7dcb0dd3b7caeba10a2
Chamaeleon <y0067315@tu-bs.de>
2019-06-04 17:37:20 +0200

    some db model fixes, kicked db.base out

--- server/app/models.py
+++ server/app/models.py
@@ -1,23 +1,23 @@
 from app import db
 from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-Base = declarative_base()
+# Base = declarative_base()
 
-room_challenge = db.Table('room_challenge', Base.metadata,
+room_challenge = db.Table('room_challenge',
     db.Column('Room_id', db.Integer, db.ForeignKey('Room.id')),
     db.Column('Challenge_id', db.Integer, db.ForeignKey('Challenge.id'))
 )
-class Room(Base):
+class Room(db.Model):
     __tablename__='Room'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     gpsposition = db.Column(db.String(200))
     points = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    # challenges = db.relationship('Challenge', secondary=room_challenge, back_populates="rooms")
+    challenges = db.relationship('Challenge', secondary=room_challenge, back_populates="rooms")
     def to_dict(self):
         data = {
             "id": self.id,
@@ -28,12 +28,12 @@ class Room(Base):
             }
         return data
 
-class Challenge(Base):
+class Challenge(db.Model):
     __tablename__='Challenge'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     official = db.Column(db.Boolean, default=False)
-    rooms = db.relationship('Room', secondary=room_challenge, backref="challenges")
+    rooms = db.relationship('Room', secondary=room_challenge, back_populates="challenges")
     def to_dict(self):
         data = {
             "id": self.id,
@@ -43,7 +43,7 @@ class Challenge(Base):
         }
         return data
 
-class User(Base):
+class User(db.Model):
     __tablename__='User'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
