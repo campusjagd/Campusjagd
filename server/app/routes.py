cc8f321d11bddb0dfa6bd91a83a768103c8d21d5
Chamaeleon <y0067315@tu-bs.de>
2019-06-04 17:46:14 +0200

    added highscores webpage

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,17 @@
 from flask import render_template
 from app import app
+from app.models import User, Challenge
+from app import models, db
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/highscores')
+def highscores():
+    scorelist = db.session.query(User).order_by(User.points.desc())
+    return render_template('highscores.html', scorelist = scorelist)
+
+
